36009ff24d2063394c25baed7afbd63312f92771
keelii <keeliizhou@gmail.com>
2019-06-15 12:42:41 +0800

    init docs

--- src/index.js
+++ src/index.js
@@ -30,13 +30,35 @@ let editor = window.editor = codemirror(document.getElementById('editor'), {
     gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]
 })
 
+const toCamelCase = str => {
+    let s = str && str
+        .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+        .map(x => x.slice(0, 1).toUpperCase() + x.slice(1).toLowerCase())
+        .join('')
+    return s.slice(0, 1).toLowerCase() + s.slice(1)
+}
+
+window.fmtOption = function() {
+    let input = document.querySelectorAll('#fmt-form input')
+    let params = {}
+    input.forEach(inp => {
+        let name = inp.getAttribute('name')
+        let value = inp.value
+        if (inp.type === 'checkbox') value = inp.checked
+        if (inp.type === 'number') value = Number(inp.value)
+
+        params[toCamelCase(name)] = value
+    })
+    return params
+}
+
 window.format = function(cm) {
-    cm.setValue(prettier.format(cm.getValue(), {
+    let options = fmtOption()
+    let defaults = {
         parser: 'babylon',
-        tabWidth: 4,
-        semi: false,
         plugins: prettierPlugins
-    }))
+    }
+    cm.setValue(prettier.format(cm.getValue(), Object.assign({}, defaults, options)))
 }
 window.clearContent = function() {
     editor.setValue('')
